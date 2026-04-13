a10e741365077f874ab1046e56f7b6c57f14549b
224145388 <224145388@CISTN2026-B005>
2026-04-13 16:07:29 +0200

    Add project files.

---
+++ CMS/Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            Login = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = Properties.Resources.login_back;
+            pictureBox1.Location = new Point(108, 50);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(282, 287);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.WhiteSmoke;
+            textBox1.Location = new Point(188, 133);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "enter username";
+            textBox1.Size = new Size(131, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.WhiteSmoke;
+            textBox2.Location = new Point(188, 200);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "enter password";
+            textBox2.Size = new Size(131, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // Login
+            // 
+            Login.AccessibleRole = AccessibleRole.MenuPopup;
+            Login.BackColor = Color.Teal;
+            Login.Cursor = Cursors.Hand;
+            Login.FlatStyle = FlatStyle.Popup;
+            Login.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Login.Location = new Point(198, 270);
+            Login.Name = "Login";
+            Login.Size = new Size(109, 25);
+            Login.TabIndex = 3;
+            Login.Text = "Login";
+            Login.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.FlatStyle = FlatStyle.System;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(215, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 37);
+            label1.TabIndex = 4;
+            label1.Text = "CMS";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SkyBlue;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(479, 382);
+            Controls.Add(label1);
+            Controls.Add(Login);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button Login;
+        private Label label1;
+    }
+}
---
+++ CMS/Login.cs
@@ -0,0 +1,40 @@
+namespace CMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
